a2e194ee6bfaad56c7712c3ef6547007f68bd500
m201368 <33030039+m201368@users.noreply.github.com>
2020-04-30 20:26:21 -0500

    the real approve

--- web/approve.php
+++ web/approve.php
@@ -9,8 +9,104 @@
   echo "<h5>ERROR: " . mysqli_connect_errno() . ": " . mysqli_connect_error() . " </h5><br>";
   }
   // Create a Page object and set the Page title to "Approve Weekends"
+  function submitApproved($db, $approvedWeekendsFinal) {
+    foreach($approvedWeekendsFinal as $alpha => $wID) {
+      $tempID = $wID;
+      $num = 1;
 
-    #if($username == 200000){
+      $query = "INSERT INTO approved (wID, alpha, approved)
+                VALUES (?,?,?)";
+      $stmt = $db->stmt_init();
+      $stmt->prepare($query);
+      $stmt->bind_param('iii', $tempID, $alpha, $num);
+      #$page->content .= $stmt;
+      $success = $stmt->execute();
+    }
+  }
+
+  function submitNotApproved($db, $notApproved) {
+    foreach($notApproved as $alpha => $wID) {
+      $tempID = $wID;
+      $num = 2;
+
+      $query = "INSERT INTO approved (wID, alpha, approved)
+                VALUES (?,?,?)";
+      $stmt = $db->stmt_init();
+      $stmt->prepare($query);
+      $stmt->bind_param('iii', $tempID, $alpha, $num);
+      #$page->content .= $stmt;
+      $success = $stmt->execute();
+    }
+  }
+
+  function getApprovedFinal($approvedAlphas, $weekendsLeft, $wID) {
+    $approvedWeekendsFinal = array();
+    $size = count($weekendsLeft);
+
+    for ($x = 0; $x < $size; $x+=1) {
+      if($weekendsLeft[$x] != 0) {
+        $approvedWeekendsFinal[$approvedAlphas[$x]] = $wID[$x];
+      }
+    }
+    return $approvedWeekendsFinal;
+  }
+
+  function getNotApproved($approvedAlphas, $weekendsLeft, $wID) {
+    $notApproved = array();
+    $size = count($weekendsLeft);
+
+    for ($x = 0; $x < $size; $x+=1) {
+      if($weekendsLeft[$x] == 0) {
+        $notApproved[$approvedAlphas[$x]] = $wID[$x];
+      }
+    }
+    return $notApproved;
+  }
+
+  function getWID($db, $approvedAlphas) {
+    $allwID = array();
+    foreach ($approvedAlphas as $alpha){
+      $query = "SELECT wID
+                  FROM midweekend
+                  WHERE alpha = ?";
+        $stmt = $db->stmt_init();
+        $stmt->prepare($query);
+        $stmt->bind_param('i', $alpha);
+        $success = $stmt->execute();
+        if (!$success || $db->affected_rows == 0) {
+        $page->content .= "<h5>ERROR: " . $db->error . " for query *$query*</h5><hr>";
+        }
+
+        $stmt->bind_result($wID);
+        while ($row = $stmt->fetch()) {
+          array_push($allwID,  $wID);
+        }
+      }
+      return $allwID;
+    }
+
+    function getWeekendsLeft($db, $approvedAlphas) {
+      $allWeekendsLeft = array();
+      foreach ($approvedAlphas as $alpha){
+        $query = "SELECT weekends_left
+                    FROM weekends_left
+                    WHERE alpha = ?";
+          $stmt = $db->stmt_init();
+          $stmt->prepare($query);
+          $stmt->bind_param('i', $alpha);
+          $success = $stmt->execute();
+          if (!$success || $db->affected_rows == 0) {
+            $page->content .= "<h5>ERROR: " . $db->error . " for query *$query*</h5><hr>";
+          }
+
+          $stmt->bind_result($weekendsLeft);
+          while ($row = $stmt->fetch()) {
+            array_push($allWeekendsLeft, $weekendsLeft);
+          }
+        }
+        return $allWeekendsLeft;
+      }
+  if($username == 200000){
       $page = new Page("Approve Weekends");
 
   // Add company submission to the page
@@ -141,7 +237,22 @@ $company=$_POST['company'];
       </div></div></div>';
 
       if (!isset($_POST['company'])) {
-        $company = getCompany($db, $approvedAlphas[0]);
+
+        $query = "SELECT Company
+                  FROM Company
+                  WHERE alpha = ?;";
+
+          $stmt = $db->stmt_init();
+          $stmt->prepare($query);
+          $stmt->bind_param('i', $approvedAlphas[0]);
+          $success = $stmt->execute();
+          if (!$success || $db->affected_rows == 0) {
+          $page->content .= "<h5>ERROR: " . $db->error . " for query *$query*</h5><hr>";
+          }
+          $stmt->bind_result($co);
+          $stmt->fetch();
+          $company=$co;
+
       }
 
       if (isset($_POST['company']) || $company != null) {
@@ -171,8 +282,6 @@ $company=$_POST['company'];
           $stmt->prepare($query);
           $stmt->bind_param('i', $company);
           $success = $stmt->execute();
-
-
           $stmt->bind_result($alpha, $firstname, $lastname, $wID, $approved);
 
 
@@ -200,129 +309,15 @@ $company=$_POST['company'];
         }
 
 
-      function submitApproved($db, $approvedWeekendsFinal) {
-        foreach($approvedWeekendsFinal as $alpha => $wID) {
-          $tempID = $wID;
-          $num = 1;
 
-          $query = "INSERT INTO approved (wID, alpha, approved)
-                    VALUES (?,?,?)";
-          $stmt = $db->stmt_init();
-          $stmt->prepare($query);
-          $stmt->bind_param('iii', $tempID, $alpha, $num);
-          #$page->content .= $stmt;
-          $success = $stmt->execute();
-        }
-      }
 
-      function submitNotApproved($db, $notApproved) {
-        foreach($notApproved as $alpha => $wID) {
-          $tempID = $wID;
-          $num = 2;
 
-          $query = "INSERT INTO approved (wID, alpha, approved)
-                    VALUES (?,?,?)";
-          $stmt = $db->stmt_init();
-          $stmt->prepare($query);
-          $stmt->bind_param('iii', $tempID, $alpha, $num);
-          #$page->content .= $stmt;
-          $success = $stmt->execute();
-        }
-      }
-
-      function getApprovedFinal($approvedAlphas, $weekendsLeft, $wID) {
-        $approvedWeekendsFinal = array();
-        $size = count($weekendsLeft);
-
-        for ($x = 0; $x < $size; $x+=1) {
-          if($weekendsLeft[$x] != 0) {
-            $approvedWeekendsFinal[$approvedAlphas[$x]] = $wID[$x];
-          }
-        }
-        return $approvedWeekendsFinal;
-      }
-
-      function getNotApproved($approvedAlphas, $weekendsLeft, $wID) {
-        $notApproved = array();
-        $size = count($weekendsLeft);
-
-        for ($x = 0; $x < $size; $x+=1) {
-          if($weekendsLeft[$x] == 0) {
-            $notApproved[$approvedAlphas[$x]] = $wID[$x];
-          }
-        }
-        return $notApproved;
-      }
-
-      function getWID($db, $approvedAlphas) {
-        $allwID = array();
-        foreach ($approvedAlphas as $alpha){
-          $query = "SELECT wID
-                      FROM midweekend
-                      WHERE alpha = ?";
-            $stmt = $db->stmt_init();
-            $stmt->prepare($query);
-            $stmt->bind_param('i', $alpha);
-            $success = $stmt->execute();
-            if (!$success || $db->affected_rows == 0) {
-            $page->content .= "<h5>ERROR: " . $db->error . " for query *$query*</h5><hr>";
-            }
-
-            $stmt->bind_result($wID);
-            while ($row = $stmt->fetch()) {
-              array_push($allwID,  $wID);
-            }
-          }
-          return $allwID;
         }
 
-        function getWeekendsLeft($db, $approvedAlphas) {
-          $allWeekendsLeft = array();
-          foreach ($approvedAlphas as $alpha){
-            $query = "SELECT weekends_left
-                        FROM weekends_left
-                        WHERE alpha = ?";
-              $stmt = $db->stmt_init();
-              $stmt->prepare($query);
-              $stmt->bind_param('i', $alpha);
-              $success = $stmt->execute();
-              if (!$success || $db->affected_rows == 0) {
-                $page->content .= "<h5>ERROR: " . $db->error . " for query *$query*</h5><hr>";
-              }
-
-              $stmt->bind_result($weekendsLeft);
-              while ($row = $stmt->fetch()) {
-                array_push($allWeekendsLeft, $weekendsLeft);
-              }
-            }
-            return $allWeekendsLeft;
+          else{
+              $page = new Page("Approve Weekends");
+              $page->content = "<h1>You are not an administrator and are not allowed to use this page. </h1>";
           }
-
-          function getCompany($db, $same) {
-            $query = "SELECT Company
-                      FROM Company
-                      WHERE alpha = ?;";
-
-              $stmt = $db->stmt_init();
-              $stmt->prepare($query);
-              $stmt->bind_param('i', $same);
-              $success = $stmt->execute();
-
-
-              $stmt->bind_result($co);
-
-
-              while ($row = $stmt->fetch()) {
-                $thisCompany = $co;
-              }
-              return $thisCompany;
-          }
-        #}
-
-        #  else{
-        #      $page = new Page("Approve Weekends");
-        #      $page->content = "<h1>You are not an administrator and are not allowed to use this page. </h1>";
-        #  }
 
   #$stmt->close();
 
